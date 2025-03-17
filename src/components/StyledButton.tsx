8fc5988f87fc3cb9ecf30703efc8d7222d1c2d3c
vNemetz <vininemetz@gmail.com>
2025-03-17 02:04:25 -0300

    Fix: button scrolling

--- src/components/StyledButton.tsx
+++ src/components/StyledButton.tsx
@@ -1,24 +1,26 @@
 interface Props {
   text: string;
-  route: string;
+  route:string;
   pt?: number;
   pb?: number;
   pl?: number;
   pr?: number;
+
 }
 
-export const StyledButton = (props: Props) => {
+export const StyledButton = (props:Props) => {
   const scrollToSection = (sectionId: string) => {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   };
+
   return (
     <button className="hover:cursor-pointer font-bold bg-[#499ed6] py-[0.65rem] rounded-full hover:bg-[#273974] text-center mt-4 w-[10rem]"
-    onClick={() => scrollToSection(props.route)}
+      onClick={() => scrollToSection(props.route)}
     >
       {props.text}
     </button>
-  );
-};
+  )
+}
